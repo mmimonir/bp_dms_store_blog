51a46e3b0bda70896beb2e8b5fdfd92e9cadf458
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2025-04-22 21:11:14 +0600

    Add AdminLTE dashboard layout and initial dashboard view
    - Introduced a new layout file for AdminLTE with a sidebar and header.
    - Created a dashboard view with various statistics widgets.
    - Added a logo image to the project.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+// Import Bootstrap JS
+import "bootstrap";
+
+// Import AdminLTE JS
+import "admin-lte";
+
+// Import Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import AdminLTE CSS
+import "admin-lte/dist/css/adminlte.min.css";
+
+// Import OverlayScrollbars if you want (optional)
+import "overlayscrollbars/styles/overlayscrollbars.css";
+
+// Import FontAwesome Icons
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import "bootstrap-icons/font/bootstrap-icons.css";
