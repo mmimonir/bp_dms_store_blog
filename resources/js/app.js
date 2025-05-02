43d9ceb6227e384d3362022db31c52c470c9e3fa
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2025-05-02 20:51:45 +0600

    Remove unused Bootstrap and Bootstrap Icons imports from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,11 +6,4 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-// Import Bootstrap Icons CSS
-import "bootstrap-icons/font/bootstrap-icons.css";
-
-// Import other assets (Bootstrap and AdminLTE)
-// import 'bootstrap';
 import "admin-lte";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "admin-lte/dist/css/adminlte.min.css";
