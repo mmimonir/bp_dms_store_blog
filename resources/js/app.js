719f075a9984eae0eaaa0c6103f6d7b93bc1d939
Md Monirul Islam <mmimonir.ctg@gmail.com>
2025-05-09 22:03:07 +0600

    feat: add custom JavaScript file and integrate Font Awesome icons in dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,9 @@
 import "./bootstrap";
+import "./custom";
 
-import Alpine from "alpinejs";
-
-window.Alpine = Alpine;
-
-Alpine.start();
+// import Alpine from "alpinejs";
+// window.Alpine = Alpine;
+// Alpine.start();
 
 import "bootstrap";
 import "admin-lte";
